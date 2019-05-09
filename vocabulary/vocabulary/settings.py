e429b5d5f601a0e1751e50cf9cec0052d171540d
dcmmc <xwt97294597@gmail.com>
2019-05-10 00:40:11 +0800

    update

--- vocabulary/vocabulary/settings.py
+++ vocabulary/vocabulary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2e6o+@g=e!9#+6q_9o%bm(n=m3ioy_%8#c%w94)j@#x8h6xv8y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
