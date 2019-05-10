4d49c7641dd7dc2361ec1ff065a0c5c2cbbbd815
dcmmc <xwt97294597@gmail.com>
2019-05-10 11:40:53 +0800

    update

---
+++ centos_start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# for centos7
+export PATH="$HOME/.local/bin:$PATH"
+export LD_LIBRARY_PATH=/usr/local/lib
+pipenv run python3 ./vocabulary/manage.py makemigrations
+pipenv run python3 ./vocabulary/manage.py runserver 0.0.0.0:8081
