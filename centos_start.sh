169983c65ad5a1a55e0625414b3c5dce47ee8fbb
dcmmc <xwt97294597@gmail.com>
2019-05-10 12:50:21 +0800

    update

--- centos_start.sh
+++ centos_start.sh
@@ -4,4 +4,5 @@ export PATH="$HOME/.local/bin:$PATH"
 export LD_LIBRARY_PATH=/usr/local/lib
 pipenv run python3 ./vocabulary/manage.py makemigrations
 pipenv run python3 ./vocabulary/manage.py makemigrations vocabulary
+pipenv run python ./vocabulary/manage.py migrate
 pipenv run python3 ./vocabulary/manage.py runserver 0.0.0.0:8081
